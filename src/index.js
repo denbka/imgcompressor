562fe5c6bedde80eb5ef28f150840d067efea2c7
Denis Rybachek <deniskindo@mail.ru>
2021-03-10 22:34:21 +0300

    стабильно кадрирует под 10х15, добавил возможность выбора цвета рамки

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 document.addEventListener("DOMContentLoaded", () => {
     const output = document.querySelector('.output-container')
     const button = document.querySelector('.button-primary')
-    const link = document.querySelector('.link')
+    const changeBorderInputs = document.querySelectorAll('.change-border-inputs > label > input')
     const canvas = document.querySelector('.canvas')
     const ctx = canvas.getContext('2d')
     const height = 379
@@ -10,6 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     canvas.width = width
     canvas.height = height
+    let borderColor = '#fff'
     const getImages = async event => {
         event.preventDefault()
 
@@ -17,19 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
         const response = await axios.get('http://localhost:8000/api/messages')
         const data = response.data.data
         const imgs = []
-        // imgs.push(await convertAndCroppImage('./src/assets/eX44pZphCv0.jpg'))
+        changeBorderInputs.forEach(radio => {
+            if (radio.checked) {
+                borderColor = radio.value
+            }
+        })
         for (let directory in data) {
-            // console.log(data[directory])
-            // console.log(`${rootDirectoryPath}/${directory}`)
             data[directory].map(async filePath => {
                 console.log(filePath)
                 const img = await axios.get(`http://localhost:8000/api/uploads/${directory}/${filePath}`)
-                output.appendChild(await convertAndCroppImage(img.data.file))
+                const node = await convertAndCroppImage(img.data.file)
+                // const formData = new FormData()
+                // formData.append('image', node)
+                await axios.post(`http://localhost:8000/api/uploads/${directory}/${filePath}`, formData)
+                if (node) output.appendChild(node) //for tests
             })
         }
-        // imgs.map(img => {
-        //     output.appendChild(img)
-        // })
         button.disabled = false
     }
 
@@ -38,28 +42,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const convertAndCroppImage = (imgPath) => {
         return new Promise((resolve, reject) => {
-            ctx.beginPath()
-            ctx.rect(0, 0, width, height)
-            ctx.fillStyle = 'blue'
-            ctx.fill()
-            ctx.closePath()
             const img = new Image()
             img.src = `data:image/jpeg;base64, ${imgPath}`
             img.onload = (data) => {
-                console.log()
+                ctx.beginPath()
+                ctx.rect(0, 0, width, height)
+                ctx.fillStyle = borderColor
+                ctx.fill()
+                ctx.closePath()
                 // ctx.drawImage(img, 0, 0, 300, data.target.height * (300 / data.target.width))
                 let innerWidth = width
                 let innerHeight = height
                 let y = 0
                 let x = 0
-                if (data.target.width < data.target.height) {
-                    console.log(123)
+                if (data.target.width <= data.target.height) {
                     innerWidth = data.target.width * (height / data.target.height)
                     x = (width - innerWidth) / 2
                 } else {
-                    // innerHeight = data.target.height * (width / data.target.width)
-                    y = (height - innerHeight) / 2
+                    innerHeight = data.target.height * (width / data.target.width)
+                    if (innerHeight > height) {
+                        innerHeight = height
+                        innerWidth = data.target.width * (height / data.target.height)
+                        x = (width - innerWidth) / 2
+                    } else {
+                        y = (height - innerHeight) / 2
+                    }
                 }
+                console.log(data.target.width + `>` + data.target.height)
                 ctx.drawImage(img, x, y, innerWidth, innerHeight)
                 const imgSrc = canvas.toDataURL()
                 ctx.clearRect(0, 0, canvas.width, canvas.height)
