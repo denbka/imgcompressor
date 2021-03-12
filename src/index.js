e6749432d9a01bd15dba8bde2fe1e5ad4f8aab25
Denis Rybachek <deniskindo@mail.ru>
2021-03-12 23:11:47 +0300

    полностью отрабатывается цикл загрузка/скачивание/рендер/загрузка

--- src/index.js
+++ src/index.js
@@ -5,42 +5,49 @@ document.addEventListener("DOMContentLoaded", () => {
     const changeBorderInputs = document.querySelectorAll('.change-border-inputs > label > input')
     const canvas = document.querySelector('.canvas')
     const ctx = canvas.getContext('2d')
+    const fileReader = document.querySelector('.filereader')
+    fileReader.addEventListener('change', event => {
+        console.log(event)
+    })
     const height = 379
     const width = 569
 
     canvas.width = width
     canvas.height = height
     let borderColor = '#fff'
+
     const getImages = async event => {
         event.preventDefault()
 
         button.disabled = true
         const response = await axios.get('http://localhost:8000/api/messages')
         const data = response.data.data
-        const imgs = []
+        const formData = new FormData()
         changeBorderInputs.forEach(radio => {
             if (radio.checked) {
                 borderColor = radio.value
             }
         })
-        for (let directory in data) {
-            data[directory].map(async filePath => {
-                console.log(filePath)
-                const img = await axios.get(`http://localhost:8000/api/uploads/${directory}/${filePath}`)
-                const node = await convertAndCroppImage(img.data.file)
-                // const formData = new FormData()
-                // formData.append('image', node)
-                await axios.post(`http://localhost:8000/api/uploads/${directory}/${filePath}`, formData)
-                if (node) output.appendChild(node) //for tests
-            })
-        }
+
+
+        //чтобы асинхронный цикл стал ждать результата, нужно ставить promise.all
+        const asyncRes = await Promise.all(Object.entries(data).map(async (item, key) => {
+            const directory = item[0]
+            const files = item[1]
+            await Promise.all(files.map(async fileName => {
+                const img = await axios.get(`http://localhost:8000/api/uploads/${directory}/${fileName}`)
+                const file = await convertAndCroppImage(img.data.file, fileName, directory)
+                formData.append('image', file)
+            }))
+        }))
+        await axios.post(`http://localhost:8000/api/uploads`, formData)
         button.disabled = false
     }
 
     button.addEventListener('click', getImages)
 
 
-    const convertAndCroppImage = (imgPath) => {
+    const convertAndCroppImage = (imgPath, fileName, directory) => {
         return new Promise((resolve, reject) => {
             const img = new Image()
             img.src = `data:image/jpeg;base64, ${imgPath}`
@@ -68,14 +75,27 @@ document.addEventListener("DOMContentLoaded", () => {
                         y = (height - innerHeight) / 2
                     }
                 }
-                console.log(data.target.width + `>` + data.target.height)
+                // console.log(data.target.width + `>` + data.target.height)
                 ctx.drawImage(img, x, y, innerWidth, innerHeight)
-                const imgSrc = canvas.toDataURL()
+                const imgSrc = canvas.toDataURL('image/jpg')
                 ctx.clearRect(0, 0, canvas.width, canvas.height)
-                const renderedImg = new Image()
-                renderedImg.src = imgSrc
-                resolve(renderedImg)
+                resolve(dataURLtoFile(imgSrc,`${directory}###${fileName}`))
             }
         })
     }
 })
+
+function dataURLtoFile(dataurl, filename) {
+ 
+    var arr = dataurl.split(','),
+        mime = arr[0].match(/:(.*?);/)[1],
+        bstr = atob(arr[1]), 
+        n = bstr.length, 
+        u8arr = new Uint8Array(n);
+        
+    while(n--){
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+    
+    return new File([u8arr], filename, {type:mime});
+}
